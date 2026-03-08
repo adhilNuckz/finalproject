2e893208601b865d95dce639d8b39d17247df2bf
adhilNuckz <mikeadhil2002@gmail.com>
2026-03-09 02:53:40 +0530

    ediyed ports on  config.js

--- server-test/frontend/src/config.js
+++ server-test/frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
-export const TERMINAL_URL = import.meta.env.VITE_TERMINAL_URL || 'http://localhost:3000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5002';
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5002';
+export const TERMINAL_URL = import.meta.env.VITE_TERMINAL_URL || 'http://localhost:3002';
